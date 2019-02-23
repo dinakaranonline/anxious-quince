2d4d670632fbd1eb3a359c5ca13a57474b5b3b0a
test <test@AMB00806.local>
2019-02-24 00:47:47 +0530

    updated

--- src/main/java/io/openshift/booster/WebController.java
+++ src/main/java/io/openshift/booster/WebController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
 
+import java.security.Principal;
+import io.openshift.booster.service.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
