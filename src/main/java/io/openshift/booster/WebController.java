385ea056b07f8bc2c020db1cdb3a738cc8cdf539
test <test@AMB00806.local>
2019-02-24 00:21:53 +0530

    Updated controller

---
+++ src/main/java/io/openshift/booster/WebController.java
@@ -0,0 +1,25 @@
+package io.openshift.booster;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+public class WebController {
+
+    @GetMapping(path = "/")
+    public String index() {
+        return "external";
+    }
+
+   @GetMapping(path = "/customers")
+   public Greeting customers(@QueryParam("name") @DefaultValue("World") String name) {
+        final String message = String.format(Greeting.FORMAT, name);
+        return new Greeting(message);
+    } 
+
+}
--- src/main/java/io/openshift/booster/security/SecurityConfig.java
+++ src/main/java/io/openshift/booster/security/SecurityConfig.java
@@ -44,7 +44,7 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.authorizeRequests()
-            .antMatchers("api/greeting*")
+            .antMatchers("customers*")
             .hasRole("user")
             .anyRequest()
             .permitAll();
